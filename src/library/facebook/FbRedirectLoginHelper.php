bc9060e02d76fe4a38a68e1d3f1db2163dc15364
Mayank Grover <mayank.grover@kartrocket.com>
2016-07-18 03:06:58 +0530

    Initialization

--- src/library/facebook/FbRedirectLoginHelper.php
+++ src/library/facebook/FbRedirectLoginHelper.php
@@ -3,9 +3,9 @@ class FbRedirectLoginHelper extends \Facebook\FacebookRedirectLoginHelper {
 	var $sessionHandler = null;
 
 	protected function storeState($state) {
-		$this->sessionHandler->set_userdata('state', $state);
+		$this->sessionHandler->write('state', $state);
 	}
 	protected function loadState() {
-		return $this->state = $this->sessionHandler->userdata('state');
+		return $this->state = $this->sessionHandler->read('state');
 	}
 }
